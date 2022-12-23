a0f470b85e6ecc08787a20da4faa23a8859f219c
Thomas Bell <thomasjbell065@gmail.com>
2022-12-22 21:15:25 -0500

    Express Session Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -34,11 +36,21 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}))
 
 // this is where we'll add authentication because here is where we start to send stuff back to the client
 function auth(req, res, next) {
-        if (!req.signedCookies.user) {
+    console.log(req.session);
+
+    if (!req.session.user) {
         const authHeader = req.headers.authorization;
         if (!authHeader) {
             const err = new Error(`You have not been authenticated!`);
@@ -53,7 +65,7 @@ function auth(req, res, next) {
         const user = auth[0];
         const pass = auth[1];
         if (user === 'admin' && pass === 'password') {
-            res.cookie('user', 'admin', {signed: true});
+            req.session.user = 'admin';
             return next(); // authorized
         } else {
             const err = new Error(`You are not authorized`);
@@ -61,8 +73,8 @@ function auth(req, res, next) {
             err.status = 401;
             return next(err);
         } // test in Chrome Incognito-mode for no caching of data!
-    } else {
-        if (req.signedCookies.user === 'admin') {
+        } else {
+        if (req.session.user === 'admin') {
             return next();
         } else {
             const err = new Error(`You are not authorized`);
